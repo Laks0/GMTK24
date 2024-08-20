68fdcc888687b7194dfaffbd9691ac084eb3a129
Ignacio Iannello <48655321+Iann7@users.noreply.github.com>
2024-08-20 05:01:22 -0300

    fade in fade out?

--- Objects/scaler.gd
+++ Objects/scaler.gd
@@ -10,22 +10,19 @@ enum ScaleTypes {ENLARGE, REDUCE}
 @export var switch  : Switch
 @export var on_time : float = 5 
 
+
+
 var on := false
 
-@onready var water_stream_intro = $water_stream_intro
-@onready var gas_stream_intro = $gas_stream_intro
 @onready var water_stream_loop = $water_stream_loop
 @onready var gas_stream_loop = $gas_stream_loop
 
-@export var transition_duration = 1.00
-@export var transition_type = 1 # TRANS_SINE
-var tween_out =get_tree().create_tween()
+
+
 
 func _ready():
 	water_stream_loop.set_volume_db(linear_to_db(0.1))
-	water_stream_intro.set_volume_db(linear_to_db(0.1))
 	gas_stream_loop.set_volume_db(linear_to_db(0.1))
-	gas_stream_intro.set_volume_db(linear_to_db(0.1))
 	if is_instance_valid(switch):
 		switch.turned.connect(turn_on)
 
@@ -42,24 +39,21 @@ func _on_body_entered(body):
 
 func turn_on():
 	if scale_type==ScaleTypes.ENLARGE:
-		water_stream_intro.play()
+		$water_stream_loop.play()
+		create_tween().tween_property($water_stream_loop,"volume_db",linear_to_db(1.0),1.0)
 		pass
 	else:
-		gas_stream_intro.play()
+		$gas_stream_loop.play()
+		create_tween().tween_property($gas_stream_loop,"volume_db",linear_to_db(1.0),1.0)
 		pass
 	on = true
 	open.emit()
 	$Timer.start(on_time)
 
-#TODO:SOLO LOOP PERO CON UN FADE IN Y CON FADE OUT 
-func _on_water_stream_intro_finished():
-	water_stream_loop.play()
-
-func _on_gas_stream_intro_finished():
-	gas_stream_loop.play()
+#TODO:SOLO LOOP PERO CON UN FADE IN Y CON FADE OUT 2
 
 func _on_timer_timeout():
-	$water_stream_loop.stop()
-	$gas_stream_loop.stop()
+	create_tween().tween_property($gas_stream_loop,"volume_db",-100.0,3.0)
+	create_tween().tween_property($water_stream_loop,"volume_db",-100.0,3.0)
 	on = false
 	close.emit()
