0545ea929d182de6fdc2d95fbf536a560619e691
Ignacio Iannello <48655321+Iann7@users.noreply.github.com>
2024-08-20 07:25:33 -0300

    fix mute menu alambre

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,5 +1,5 @@
 extends Control
-
+var is_OST_muted:bool=false
 func _on_texture_button_pressed():
 	$AudioStreamPlayer.play()
 	DeathHandler.foods_obtained = []
@@ -19,3 +19,12 @@ func _on_sfx_pressed():
 func _on_soundtrack_pressed():
 	var idx = AudioServer.get_bus_index("OST")
 	AudioServer.set_bus_mute(idx, not AudioServer.is_bus_mute(idx))
+	#AUDIO SERVER NO ESTARIA FUNCIONANDO
+	if is_OST_muted:
+		is_OST_muted=false
+		$Intro.set_volume_db(linear_to_db(1.0))
+		$Loop.set_volume_db(linear_to_db(1.0))
+	else:
+		is_OST_muted=true
+		$Intro.set_volume_db(linear_to_db(0.0))
+		$Loop.set_volume_db(linear_to_db(0.0))
