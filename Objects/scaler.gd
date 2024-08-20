153441d08e0fab5e4672c91d655bc4b15f05a360
Ignacio Iannello <48655321+Iann7@users.noreply.github.com>
2024-08-20 03:58:57 -0300

    scaler stuff

--- Objects/scaler.gd
+++ Objects/scaler.gd
@@ -17,6 +17,10 @@ var on := false
 @onready var water_stream_loop = $water_stream_loop
 @onready var gas_stream_loop = $gas_stream_loop
 
+@export var transition_duration = 1.00
+@export var transition_type = 1 # TRANS_SINE
+var tween_out =get_tree().create_tween()
+
 func _ready():
 	water_stream_loop.set_volume_db(linear_to_db(0.1))
 	water_stream_intro.set_volume_db(linear_to_db(0.1))
@@ -57,3 +61,14 @@ func _on_water_stream_intro_finished():
 
 func _on_gas_stream_intro_finished():
 	gas_stream_loop.play()
+
+func fade_out(stream_player):
+	# tween music volume down to 0
+	tween_out.interpolate_property(stream_player, "volume_db", 0, -80, transition_duration, transition_type, Tween.EASE_IN, 0)
+	tween_out.start()
+	tween_out.interpolate_property
+	# when the tween ends, the music will be stopped
+
+func _on_TweenOut_tween_completed(object, key):
+	# stop the music -- otherwise it continues to run at silent volume
+	object.stop()
